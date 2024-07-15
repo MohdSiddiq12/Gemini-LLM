99be6da80caea96892cc0e8b59b9918920f95dda
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-07-15 15:41:34 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 import streamlit as st
 import google.generativeai as genai
 import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+#function to load Gemini model and get reponses
+
+model = genai.GenerativeModel('gemini-pro')
+
+def get_gemini_resonse(question):
+    response = model.generate_content(question)
+    return response.text
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini LLM Application")
+
+input = st.text_input("input: ", key = "input")
+
+
+    
