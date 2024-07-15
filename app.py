27e7967a76af9b06e5a6cdd7dec93c940f40acaa
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-07-15 15:37:32 +0530

    imports

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
-import googlee-generativeai as genai
-import python-dotenv
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
